b7d290effb9d567dfe1012ff8a315da85c28782b
Gabriel Donas <osradecky@gmail.com>
2021-04-30 22:54:30 +0100

    import global variable from lottie-web

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-//const bodymovin = require("bodymovin");
+import lottie from "lottie-web";
+import animationJson from "./data.json";
 
+const animation = lottie.loadAnimation({
+  container: document.getElementById("svgContainer"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  animationData: animationJson,
+});
+
+/*
+import bodymovin from "bodymovin"
 const animation = bodymovin.loadAnimation({
   container: document.getElementById("svgContainer"),
   renderer: "svg",
   loop: true,
   autoplay: true,
-  path: "../src/data.json",
+  path: "../src/boom.json",
 });
+*/
