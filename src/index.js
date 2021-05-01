376c9317952066c6a1b78f069c86871f648d13ae
Gabriel Donas <osradecky@gmail.com>
2021-05-01 21:04:20 +0100

    Add event listener to the animation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import lottie from "lottie-web";
-import animationJson from "./data.json";
+import animationJson from "./s.json";
+
+const container = document.getElementById("svgContainer");
 
 const animation = lottie.loadAnimation({
-  container: document.getElementById("svgContainer"),
+  container: container,
   renderer: "svg",
-  loop: true,
-  autoplay: true,
+  loop: false,
+  autoplay: false,
   animationData: animationJson,
 });
+
+const startAnimation = () => {
+  animation.play();
+};
+
+const stopAnimation = () => {
+  animation.pause();
+};
+
+const test = () => {
+  animation.goToAndPlay(0);
+};
+container.addEventListener("mouseover", startAnimation);
+//container.addEventListener("mouseover", startAnimation);
+//container.addEventListener("mouseout", stopAnimation);
 
 /*
 import bodymovin from "bodymovin"
